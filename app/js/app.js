16cd3ab6b404f21d681d6b4f5c7e0ea0aa58efb9
max-gui <playstation.gui@gmail.com>
2014-05-20 15:53:43 +0800

    begin is ok

--- app/js/app.js
+++ app/js/app.js
@@ -2,16 +2,59 @@
 
 
 // Declare app level module which depends on filters, and services
+
 angular.module('myApp', [
-  'ngRoute',
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
-  'myApp.controllers'
+  'myApp.controllers',
+  'ui.router',
+  'ui.sortable',
+  'ui.tree'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/loginPage', {templateUrl: 'partials/loginPage.html', controller: 'loginCtrl'});
-  $routeProvider.when('/funcPage/:name/:password', {templateUrl: 'partials/funcPage.html', controller: 'funcCtrl'});
-  $routeProvider.when('/storageViewPage', {templateUrl: 'partials/storageViewPage.html', controller: 'storageViewCtrl'});
-  $routeProvider.otherwise({redirectTo: '/loginPage'});
-}]);
+config(['$stateProvider', '$urlRouterProvider',
+        function($stateProvider, $urlRouterProvider) {
+          //$urlRouterProvider.when('/loginPage', {templateUrl: 'partials/loginPage.html', controller: 'loginCtrl'});
+          //$urlRouterProvider.when('/funcPage/:name/:password', {templateUrl: 'partials/funcPage.html', controller: 'funcCtrl'});
+          //$urlRouterProvider.when('/storageViewPage', {templateUrl: 'partials/storageViewPage.html', controller: 'storageViewCtrl'});
+          //$urlRouterProvider.when('/storeViewPage/:storeno', {templateUrl: 'partials/storeViewPage.html', controller: 'storeViewCtrl'});
+          //$urlRouterProvider.otherwise({redirectTo: '/loginPage'});
+
+          // For any unmatched url, redirect to /state1
+          $urlRouterProvider.otherwise("/loginPage");
+
+          //
+          // Now set up the states
+          $stateProvider
+          .state('login', {
+            url: "/loginPage",
+            templateUrl: "partials/loginPage.html",
+            controller: 'loginCtrl'
+          }).
+          state('navTemplate', {
+            url: "/navTemplate",
+            templateUrl: "partials/navTemplate.html",
+            controller: 'funcCtrl'
+          }).
+          state('navTemplate.func', {
+            url: "/funcPage/:name/:password",
+            templateUrl: "partials/funcPage.html",
+            controller: 'funcCtrl'
+          }).
+          state('navTemplate.storageView', {
+            url: "/storageViewPage",
+            templateUrl: "partials/storageViewPage.html",
+            controller: 'storageViewCtrl'
+          }).
+          state('navTemplate.storeView', {
+            url: "/storeViewPage/:storeno",
+            templateUrl: "partials/storeViewPage.html",
+            controller: 'storeViewCtrl'
+          });
+        }]).
+directive('popover', function() {
+  return function(scope, elem) {
+    elem.popover();
+  }
+});
+
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,126 +2,263 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('loginCtrl', ['$scope','$http',
-    function($scope, $http) {
 
-  }])
-  .controller('funcCtrl', ['$scope', '$routeParams','$http',
-    function($scope, $routeParams, $http) {
-      $scope.name = $routeParams.name;
-      $scope.password = $routeParams.password;
+angular.module('myApp.controllers', []).directive('popover', function() {
+  return function(scope, elem) {
+    elem.popover();
+  }
+})
+.controller('loginCtrl', ['$scope','$http',
+                          function($scope, $http) {
 
-      $http.get('data/up.json').success(function(data) {
-        $scope.res = data;
-      });
+                          }])
+.controller('funcCtrl', ['$scope', '$rootScope', '$state', '$http',
+                         function($scope, $rootScope, $state, $http) {
+                           $rootScope.name = $state.params.name;
+                           $rootScope.password = $state.params.password;
 
-  }])
-  .controller('storageViewCtrl', ['$scope', '$routeParams','$http',
-    function($scope, $routeParams, $http) {
-      $scope.storageNum = [1,2,3,4];
-      $scope.tabData =
-        [
-          {
-            "row": 1,
-            "col": 1,
-            "number": "11",
-            "num": 10,
-            "state": 1
-          }
-        ];
+                           //$http.defaults.useXDomain = true;
+                           //$http.defaults.headers.ContentType = "text/plain, charset=utf-8";
 
-      var tabData =
-          [
-            {
-              "row": 1,
-              "col": 2,
-              "number": "12",
-              "num": 10,
-              "state": 0
-            },
-            {
-              "row": 2,
-              "col": 1,
-              "number": "21",
-              "num": 10,
-              "state": 1
-            },
-            {
-              "row": 2,
-              "col": 2,
-              "number": "22",
-              "num": 10,
-              "state": 0
-            },
-            {
-              "row": 3,
-              "col": 2,
-              "number": "22",
-              "num": 10,
-              "state": 0
-            },
-            {
-              "row": 1,
-              "col": 1,
-              "number": "11",
-              "num": 10,
-              "state": 1
-            }
+                           $http.get('http://192.168.58.122/SYCService/Login(' +
+                                     $state.params.name + ',' +
+                                     $state.params.password + ')'
+                                    ).
+                           success(function(data) {
+                             if(data.user_name == undefined){
+                               $state.go("login");
+                             }
+                           });
 
-          ];
+                         }])
+.controller('storageViewCtrl', ['$scope', '$stateParams','$http',
+                                function($scope, $stateParams, $http) {
+                                  $scope.storageNum = [1,2,3,4];
+                                  /*
+                                  $scope.tabData =
+                                    [
+                                    {
+                                      "row": 1,
+                                      "col": 1,
+                                      "number": "1",
+                                      "num": 10,
+                                      "state": 1
+                                    }
+                                  ];
 
+                                  var tabData =
+                                      [
+                                        {
+                                          "row": 1,
+                                          "col": 2,
+                                          "number": "1",
+                                          "num": 10,
+                                          "state": 0
+                                        },
+                                        {
+                                          "row": 2,
+                                          "col": 1,
+                                          "number": "1",
+                                          "num": 10,
+                                          "state": 1
+                                        },
+                                        {
+                                          "row": 2,
+                                          "col": 2,
+                                          "number": "1",
+                                          "num": 10,
+                                          "state": 0
+                                        },
+                                        {
+                                          "row": 3,
+                                          "col": 2,
+                                          "number": "1",
+                                          "num": 10,
+                                          "state": 0
+                                        },
+                                        {
+                                          "row": 1,
+                                          "col": 1,
+                                          "number": "1",
+                                          "num": 10,
+                                          "state": 1
+                                        }
 
-      function mySortRow(a,b){
-        return a.row - b.row;
-      }
-      function mySortCol(a,b){
-        return a.Col - b.Col;
-      }
-      tabData.sort(mySortRow);
-      tabData.sort(mySortCol);
-      tabData;
-      var tabColLeft = [],tabColRight = [];
-      var tabDataLR = [];
-      tabData.forEach(
-        function(data){
-          var tmp = new Object();
+                                      ];
 
-          if(data.state == 0)
-          {
-            data.state = "success";
-          }
-          else
-          {
-            data.state = "danger";
-          }
+                                  tabData.sort(mySortRow);
+                                  tabData.sort(mySortCol);
+                                  tabData;
+                                  var tabColLeft = [],tabColRight = [];
+                                  var tabDataLR = [];
+                                  tabData.forEach(
+                                    function(data){
+                                      var tmp = new Object();
 
-          if(data.col == 1)
-          {
-            tmp.left = data;
-          }
-          else
-          {
-            tmp.right = data;
-          }
-          tabDataLR.push(tmp);
-        }
-      );
-      tabColLeft;
-      tabColRight;
-      tabDataLR;
-      $scope.tabDataLR = tabDataLR;
+                                      if(data.state == 0)
+                                      {
+                                        data.state = "success";
+                                      }
+                                      else
+                                      {
+                                        data.state = "danger";
+                                      }
 
-      $scope.getStorageInfo = function(pileno) {
-        $http.get('data/pilo' + pileno + '.json').success(function(data) {
-        $scope.tabData = data;
-      });
-    }
+                                      if(data.col == 1)
+                                      {
+                                        tmp.left = data;
+                                        tabColLeft.push(data);
+                                      }
+                                      else
+                                      {
+                                        tmp.right = data;
+                                        tabColRight.push(data);
+                                      }
+                                      tabDataLR.push(tmp);
+                                    }
+                                  );
+                                  tabColLeft;
+                                  tabColRight;
+                                  tabDataLR;
+                                  //$scope.tabDataLR = tabDataLR;
+                                  $scope.tabDataLR = [];
+                                  $scope.tabDataLR.push(tabColLeft);
+                                  $scope.tabDataLR.push(tabColRight);
 
-  }])
-  .controller('MyCtrl1', [function() {
+                                  $scope.getStorageInfo = function(pileno) {
+                                    $http.get('data/pilo' + pileno + '.json').success(function(data) {
+                                      $scope.tabData = data;
+                                    });
+                                  }
+*/
+                                  function mySortRow(a,b){
+                                    return a.row_no - b.row_no;
+                                  }
 
-  }])
-  .controller('MyCtrl2', [function() {
+                                  function mySortCol(a,b){
+                                    return a.col_no - b.col_no;
+                                  }
 
-  }]);
+                                  $scope.getStorageInfo = function(pileno) {
+                                    $http.get('http://192.168.58.122/SYCService/GetPilesByHallno(' + pileno + ')').success(function(data) {
+                                      var tabData= data;
+                                      tabData.sort(mySortRow);
+                                      //tabData.sort(mySortCol);
+                                      var tabColLeft = [],tabColRight = [];
+                                      var tabDataLR = [];
+                                      tabData.forEach(
+                                        function(data){
+                                          var tmp = new Object();
+
+                                          if(data.lock_status  == 0)
+                                          {
+                                            data.state = "success";
+                                          }
+                                          else
+                                          {
+                                            data.state = "danger";
+                                          }
+
+                                          if(data.col_no == 1)
+                                          {
+                                            tmp.left = data;
+                                            tabColLeft.push(data);
+                                          }
+                                          else
+                                          {
+                                            tmp.right = data;
+                                            tabColRight.push(data);
+                                          }
+                                          tabDataLR.push(tmp);
+                                        }
+                                      );
+
+                                      //$scope.tabDataLR = tabDataLR;
+                                      $scope.tabDataLR = [];
+                                      $scope.tabDataLR.push(tabColLeft);
+                                      $scope.tabDataLR.push(tabColRight);
+                                    });
+                                  }
+
+                                }])
+.controller('storeViewCtrl', ['$scope', '$stateParams','$http',
+                              function($scope, $stateParams, $http) {
+
+                                //$("[data-toggle=popover]").popover();
+                                $scope.storeno = $stateParams.storeno;
+                                $scope.rowNum = [1];
+                                $scope.choosenRow = $scope.rowNum[0];
+                                $scope.toGetData = [
+                                  {
+                                    SLAB_NO : ""
+                                  }
+                                ];
+                                $scope.rr = [];
+
+                                $http.get('data/store' + $scope.storeno + '.json').success(function(data) {
+                                  data.sort(mySortTIER_NO);
+                                  $scope.storeData = data;
+
+                                  data.forEach(
+                                    function(elem,index)
+                                    {
+                                      $scope.rowNum.push(index + 2);
+                                    }
+                                  )
+                                });
+
+                                $scope.setChoosenRow = function(row)
+                                {
+                                  $scope.choosenRow = row;
+                                }
+
+                                $scope.addToGet = function() {
+                                  $scope.toGetData.push({SLAB_NO:""});
+                                };
+
+                                $scope.remove = function(scope) {
+                                  //scope.remove();
+                                };
+
+                                $scope.myRemove = function(scope)
+                                {
+                                  scope.remove();
+                                  $scope.rowNum.pop();
+                                };
+
+                                $scope.add = function(SLAB_NO, TIER_NO) {
+                                  $scope.storeData.splice($scope.storeData.length + 1 - TIER_NO,0,
+                                                          {
+                                                            "SLAB_NO": SLAB_NO,
+                                                            "STEEL_GRADE": "Q345GJC-Z15",
+                                                            "SLAB_THICK": 300,
+                                                            "SLAB_WIDTH": 1810,
+                                                            "SLAB_LENGTH": 2840,
+                                                            "SLAB_WEIGHT": 11982,
+                                                            "PILE_NO": "H1022",
+                                                            "TIER_NO": TIER_NO,
+                                                            "COIL_NO": "",
+                                                            "ST_NO": "",
+                                                            "SLAB_STATUS": "000",
+                                                            "SLAB_ATTRIBUTE": "00000000",
+                                                            "HCR_FLAG": 0,
+                                                            "HEAT_NO": "",
+                                                            "YC_FLAG": 0,
+                                                            "ORDER_NO": "",
+                                                            "ACT_WEIGHT": 0,
+                                                            "YARD_NO": "",
+                                                            "PLANT_NO": 1
+                                                          }).sort(mySortTIER_NO);
+                                  $scope.rowNum.push($scope.storeData.length + 1);
+                                };
+
+                                function mySortTIER_NO(a,b){
+                                  return b.TIER_NO - a.TIER_NO;
+                                }
+                              }])
+.controller('MyCtrl1', [function() {
+
+}])
+.controller('MyCtrl2', [function() {
+
+}]);
